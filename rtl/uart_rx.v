2657ce3dea3fe774a515e7f23c53bb8f7e968232
syule <94171817+EropRHu4@users.noreply.github.com>
2022-11-07 18:01:42 +0300

    Update uart_rx.v

--- rtl/uart_rx.v
+++ rtl/uart_rx.v
@@ -1,24 +1,45 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.11.2022 11:33:20
+// Design Name: 
+// Module Name: uart_rx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module uart_rx(
 
 input          clk,
 input          enable,
 input          in,
-output  [7:0]  data_out,
-output         done,
-output         busy,
-output         error
+output   reg  [7:0]  data_out,
+output   reg      done,
+output   reg      busy,
+output   reg      error
 
     );
-    
+
 reg [1:0] state;
 reg [3:0] cnt = 0;
-// reg [7:0] data;
+reg [7:0] data;
 
 parameter   IDLE   = 2'b00,
-            START  = 2'b01,
             R_DATA = 2'b10,
             STOP   = 2'b11;
-          
+
 always @(posedge clk) begin
     case(state)
         
@@ -28,17 +49,43 @@ always @(posedge clk) begin
             done <= 0;
             busy <= 0;
             error <= 0;
-            if (enable && in) begin
-                state <= START;
-                busy <= 1;
+            cnt <= 0;
+            if (enable) begin
+                if (in == 1) begin
+                    error <= 1;
+                    state <= IDLE;
+                end
+                else begin
+                    state <= R_DATA;
+                    busy <= 1;
+                end
             end
         end
         
         R_DATA: begin
-            if (cnt != 4'b0000 && cnt <= 4'b1000)
-                data_out[cnt] <= in;
-            
+            data[cnt] <= in;
+            if (&cnt == 0)
+                cnt <= cnt + 1;
+            else begin
+                cnt <= 0;
+                state <= STOP;
+            end
         end
+            
+        STOP: begin
+            if (in == 0)
+                error <= 1;
+            else begin
+                busy <= 0;
+                done <= 1;
+                state <= IDLE;
+                data <= 0;
+                data_out <= data;
+            end
+        end
+        
+        default: state <= IDLE;
+        
     endcase
 end   
     
