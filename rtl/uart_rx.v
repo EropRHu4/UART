c5757af7a157e0fbb8593b4f7b2804117516333d
syule <94171817+EropRHu4@users.noreply.github.com>
2023-01-09 15:01:21 +0300

    Update uart_rx.v
    add BaudGenerator
    fixed some mistakes

--- rtl/uart_rx.v
+++ rtl/uart_rx.v
@@ -24,7 +24,6 @@ module uart_rx(
 
 input                   clk,
 input                   rst_n,
-input                   enable_clk,
 input                   valid,
 input                   in,
 output   reg  [7:0]     data_out,
@@ -32,9 +31,17 @@ output   reg            rx_ready
 
     );
 
+BaudGenerator   BaudGenerator
+(
+ .clk           (clk),
+ .baud_en       (|state),
+ .enable_clk    (enable_clk)
+);
+
 reg [1:0] state;
-reg [2:0] cnt = 0; // count of bits;
+reg [3:0] cnt = 0; // count of bits;
 reg [7:0] data;    // recieved data;
+wire enable_clk;
 
 
 parameter   IDLE   = 2'b00,
@@ -44,21 +51,21 @@ parameter   IDLE   = 2'b00,
 always @(posedge clk) begin
     case(state)
     
-    IDLE: if (enable_clk && valid) begin
+    IDLE: if (valid) begin
                 if (in == 0) begin
                     state <= R_DATA;
                 end
                 else state <= IDLE;
           end
     
-    R_DATA: if (enable_clk && &cnt) begin
+    R_DATA: if (enable_clk && cnt == 4'b1010) begin
                 state <= STOP;
             end
-            
+    
     STOP: if (enable_clk) begin
                 state <= IDLE;
           end
-          
+    
     default: if (enable_clk) state <= IDLE;
     
     endcase
@@ -71,17 +78,15 @@ always @(posedge clk) begin
         rx_ready <= 0;
     end    
     case(state)
-        
-        IDLE: if (enable_clk && valid) begin
-                //data_out <= 0;
-                //rx_ready <= 0;
+
+        IDLE: if ( valid ) begin
                 cnt <= 0;
                 data <= 0;
         end
 
         R_DATA: if (enable_clk) begin
                     data[cnt] <= in;
-                    if (&cnt == 0)
+                    if (cnt != 4'b1010)
                         cnt <= cnt + 1;
                     else begin
                         cnt <= 0;
@@ -95,6 +100,7 @@ always @(posedge clk) begin
         end
 
     endcase
-end   
+end
 
 endmodule
+
