31cb70332efffb80d3b507a8172d1f7d49eeb090
syule <94171817+EropRHu4@users.noreply.github.com>
2023-02-01 14:51:05 +0300

    Update uart_rx.v
    new version

--- rtl/uart_rx.v
+++ rtl/uart_rx.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 03.11.2022 11:33:20
+// Create Date: 23.01.2023 15:18:22
 // Design Name: 
 // Module Name: uart_rx
 // Project Name: 
@@ -23,86 +23,131 @@
 module uart_rx(
 
 input                   clk,
+input                   rx_clk,
 input                   rst_n,
-input                   valid,
+input                   rx_enabled,
 input                   in,
 output   reg  [7:0]     data_out,
-output   reg            rx_ready
+output   reg            rx_ready,
+output   reg  [15:0]    LED
 
     );
 
-reg [1:0] state;
+
+parameter   RESET    = 2'b00, 
+            IDLE     = 2'b01,
+            R_DATA   = 2'b10,
+            STOP_BIT = 2'b11;
+
+
+reg [1:0] state = RESET;
+reg [1:0] next = RESET;
 reg [3:0] cnt = 0; // count of bits;
-reg [7:0] data;    // recieved data;
-wire enable_clk;
+reg [7:0] data = 0;    // recieved data;
+reg rx_parity = 0;
+reg [1:0] inputSw = 2'b0;
 
-
-BaudGenerator   BaudGenerator
-(
- .clk           (clk),
- .baud_en       (|state),
- .enable_clk    (enable_clk)
-);
-
-
-parameter   IDLE   = 2'b00,
-            R_DATA = 2'b01,
-            STOP   = 2'b10;
-
-always @(posedge clk) begin
-    case(state)
-    
-    IDLE: if (valid) begin
-                if (in == 0) begin
-                    state <= R_DATA;
-                end
-                else state <= IDLE;
-          end
-    
-    R_DATA: if (enable_clk && cnt == 4'b1010) begin
-                state <= STOP;
-            end
-    
-    STOP: if (enable_clk) begin
-                state <= IDLE;
-          end
-    
-    default: if (enable_clk) state <= IDLE;
-    
-    endcase
+initial begin
+    LED <= 0;
+    rx_ready <= 0;
+    data_out <= 0;
 end
 
-always @(posedge clk) begin
-    if (!rst_n) begin
-        state <= IDLE;
-        data_out <= 0;
-        rx_ready <= 0;
-    end    
+function set_state;                                                                   
+    input [1:0] new_state;                                  
+    begin                                                                            
+
+    next = new_state;
+    LED[15:14] = new_state;       
+    if (new_state == IDLE)
+        LED[12] = 1'b1;
+    else
+        LED[12] = 1'b0;
+    if (new_state == R_DATA)
+        LED[11] = 1'b1;
+    else
+        LED[11] = 1'b0;
+
+end                                                                                  
+endfunction       
+
+
+always @(posedge rx_clk) 
+//if (rx_enabled) 
+begin
+    if (!rst_n) state <= RESET;
+    else state <= next;
+end
+
+always @(posedge rx_clk)
+ //if (rx_enabled) 
+ begin
+    if (in)
+       LED[9] <= 1'b1;
+    else
+       LED[8] <= 1'b1;
+
+    inputSw = { inputSw[0], in };
+    //LED[15:14] <= state;
+    LED[10] <= ~LED[10];
     case(state)
 
-        IDLE: if ( valid ) begin
-                cnt <= 0;
-                data <= 0;
+        RESET: begin
+            data_out <= 0;
+            rx_ready <= 0;
+            cnt <= 0;
+            data <= 0;
+            inputSw = 2'b0;
+            rx_parity = 0;
+            LED[7:0] <= 0;
+//            if (valid)begin
+                set_state(IDLE); // next = IDLE:
+//            end
         end
 
-        R_DATA: if (enable_clk) begin
-                    data[cnt] <= in;
-                    if (cnt != 4'b1010)
-                        cnt <= cnt + 1;
-                    else begin
-                        cnt <= 0;
-                    end
+        IDLE: begin
+                LED[7:0] <= 0;
+                data_out <= 0;
+                rx_ready <= 0;
+                if (inputSw == 2'b10) begin
+                    cnt <= 0;
+                    set_state(R_DATA); //next = R_DATA;
+                end
         end
 
-        STOP: if (enable_clk) begin
-                data <= 0;
-                data_out <= data;
-                rx_ready <= 1;
-        end
+        R_DATA:
+           begin
+              data[cnt] <= in;
+              if (in)
+                LED[cnt] <= 1'b1;
+              else
+                LED[cnt] <= 1'b0;
 
+              if (cnt == 4'b0111) begin
+                  set_state(STOP_BIT);//next = STOP_BIT;
+              end
+              cnt <= cnt + 1;
+           end
+
+       STOP_BIT: begin
+              if (cnt == 4'b1000) begin  
+                rx_parity = ^data[7:0] == 1 ? 1 : 0; // EVEN parity
+                //  check parity, if failed state <= RESET 
+                if (in != rx_parity)
+                    set_state(RESET);//next = RESET;
+              end
+              else begin //if (cnt = 4'b1001)
+                if (!in) //                in  должен быть 1 иначе - RESET== 0??
+                    set_state(RESET);//next = RESET; 
+                else begin
+                    data_out <= data;
+                    rx_ready <= 1;
+                    set_state(IDLE);//next = IDLE;
+                end
+              end
+              cnt <= cnt + 1;
+       end
     endcase
 end
 
 endmodule
-
-
